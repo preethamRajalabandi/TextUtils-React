e77e134db2ed96f9439dc6a59c2d38ce8697d5bc
preethamRajalabandi <preethamrajalabandi007@gmail.com>
2023-07-01 21:28:37 +0530

    bug fix, video 19

--- src/App.js
+++ src/App.js
@@ -36,24 +36,24 @@ function App() {
   }
   return (
     <>
-    <BrowserRouter>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-      {/* <Navbar/> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        
-          <Routes>
-            <Route 
-            exact path="/" 
-            element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/> }/>
+      <BrowserRouter>
+        {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+        {/* <Navbar/> */}
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
 
-            <Route 
-            exact path="/about" 
-            element={<About/>}/>
+          <Routes>
+            <Route
+              exact path="/"
+              element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra Spaces" mode={mode} />} />
+
+            <Route
+              exact path="/about"
+              element={<About mode={mode} />} />
           </Routes>
-       
-      </div>
+
+        </div>
       </BrowserRouter>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,6 +31,7 @@ export default function TextForm(props) {
         const copyText = document.getElementById("myBox")
         copyText.select();
         navigator.clipboard.writeText(copyText.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard!", "success")
         text.length > 0 ? props.showAlert("Copied to Clipboard!", "success") : props.showAlert("No text to copy, Please add some text!", "warning")
     }
@@ -44,23 +45,23 @@ export default function TextForm(props) {
     return (
         <>
             <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
-                <h1>{props.heading}</h1>
+                <h1 className='mb-4'>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{ backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'dark' ? 'white' : 'black' }} ></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{ backgroundColor: props.mode === 'light' ? 'white' : '#13466e', color: props.mode === 'dark' ? 'white' : 'black' }} ></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to UpperCase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLowClick} >Convert to LowerCase</button>
-                <button className="btn btn-primary mx-1" onClick={handleClearText} >Clear Text</button>
-                <button className="btn btn-primary mx-1" onClick={handleCopy} >Copy Text</button>
-                <button className="btn btn-primary mx-1" onClick={handleRemoveExtraSpaces} >Remove Extra Spaces</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick} >Convert to UpperCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLowClick} >Convert to LowerCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearText} >Clear Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleRemoveExtraSpaces} >Remove Extra Spaces</button>
             </div>
             <div className="container my-2" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").length - 1} words and {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length} minutes to read</p>
+                <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} minutes to read</p>
 
                 <h2>Preview</h2>
-                <p>{text.length > 0 ? text : "Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length > 0 ? text : "Nothing to preview"}</p>
             </div>
         </>
     )
